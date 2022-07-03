fc1933b469c3e002a99be8d9f365556cc397bb13
chicco4 <chiccotto4@gmail.com>
2022-07-03 15:18:50 +0200

    skeletons start

---
+++ script.js
@@ -0,0 +1,7 @@
+/** 
+ * Your main goal here is to have as little global code as possible.
+ * Try tucking everything away inside of a module or factory.
+ * Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module.
+ * If you need multiples of something (players!), create them with factories.
+*/
+
